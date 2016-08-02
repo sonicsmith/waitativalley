9aa1d03ddbe5ba5268b26b27a0ea55561bdcf5aa
sonicsmith <thesonicsmith@gmail.com>
2016-08-02 20:24:52 +1200

    Background Image
    Repositions image if screen is not wide. Also fixed page offset.

--- assets/js/main.js
+++ assets/js/main.js
@@ -135,10 +135,10 @@
 				offset: function() {
 
 					// If <=large, >small, and sidebar is present, use its height as the offset.
-						// if (skel.breakpoint('large').active
-						// &&	!skel.breakpoint('small').active
-						// &&	$sidebar.length > 0)
-						// 	return $sidebar.height();
+						if (skel.breakpoint('large').active
+						&&	!skel.breakpoint('small').active
+						&&	$sidebar.length > 0)
+							return $sidebar.height();
 
 					return 0;
 
